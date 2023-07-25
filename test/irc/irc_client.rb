d341b8d9562c6a7db8b4db5b896eee5d0d646049
Pankaj Sharma <sharmapankaj2512@gmail.com>
2023-07-25 14:50:06 +0400

    tell don't ask

--- test/irc/irc_client.rb
+++ test/irc/irc_client.rb
@@ -4,15 +4,18 @@ class IrcClient
   def initialize(host, port)
     @host = host
     @port = port
+    @lines = ""
   end
 
   def connect
     socket = TCPSocket.open(@host, @port)
-    lines = ""
     while (line = socket.gets) # Read lines from the socket
-      lines += line.chop
+      @lines += line.chop
     end
     socket.close
-    lines
+  end
+
+  def connected
+    @lines.include? "NOTICE"
   end
 end
--- test/irc/irc_connect_test.rb
+++ test/irc/irc_connect_test.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require_relative 'irc_client'
+require_relative "irc_client"
 require "minitest/autorun"
-require 'socket'
+require "socket"
 
 class IrcConnectTest < Minitest::Test
   def test_connection
     client = IrcClient.new("irc.libera.chat", 6667)
-    response = client.connect
+    client.connect
 
-    assert_includes response, "NOTICE"
+    assert client.connected
   end
 end
