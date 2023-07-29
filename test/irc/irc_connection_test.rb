a51b6fdf1760121331afb6186b6db31edadb4b86
Pankaj Sharma <sharmapankaj2512@gmail.com>
2023-07-29 17:53:59 +0400

    connect

--- test/irc/irc_connection_test.rb
+++ test/irc/irc_connection_test.rb
@@ -1,15 +1,31 @@
 require 'minitest/autorun'
+require 'socket'
 
 class IrcConnectionTest < Minitest::Test
-  def test_client_connects
+  def test_client_not_connected
     client = IrcClient.new
 
     refute client.connected
   end
+
+  def test_client_connect
+    client = IrcClient.new("irc.libera.chat", 6667)
+
+    assert client.connected
+  end
 end
 
 class IrcClient
+  def initialize(host, port)
+    @socket = TCPSocket.new(host, port)
+    until @socket.eof?
+      line = @socket.gets
+      @connected = line.include? "NOTICE"
+      break if @connected
+    end
+  end
+
   def connected
-    # code here
+    @connected
   end
 end
