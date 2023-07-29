75fea7feec4a96fafad52df36ba6affec3574377
Pankaj Sharma <sharmapankaj2512@gmail.com>
2023-07-29 18:01:27 +0400

    invalid port

--- test/irc/irc_connection_test.rb
+++ test/irc/irc_connection_test.rb
@@ -1,31 +1,44 @@
 require 'minitest/autorun'
 require 'socket'
+require 'timeout'
 
 class IrcConnectionTest < Minitest::Test
-  def test_client_not_connected
-    client = IrcClient.new
-
-    refute client.connected
-  end
-
   def test_client_connect
     client = IrcClient.new("irc.libera.chat", 6667)
 
     assert client.connected
   end
+
+  def test_invalid_port
+    client = IrcClient.new("irc.libera.chat", 666)
+
+    refute client.connected
+  end
 end
 
 class IrcClient
   def initialize(host, port)
-    @socket = TCPSocket.new(host, port)
-    until @socket.eof?
-      line = @socket.gets
-      @connected = line.include? "NOTICE"
-      break if @connected
+    begin
+      @socket = connect(host, port)
+      until @socket.eof?
+        line = @socket.gets
+        @connected = line.include? "NOTICE"
+        break if @connected
+      end
+    rescue Timeout::Error
+      @connected = false
     end
   end
 
   def connected
     @connected
+  end
+
+  private
+
+  def connect(host, port)
+    Timeout::timeout(10) do
+      TCPSocket.new(host, port)
+    end
   end
 end
