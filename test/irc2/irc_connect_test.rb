a40ccd84ba55e678673eee04dfa065fb76a493f4
Pankaj Sharma <sharmapankaj2512@gmail.com>
2023-09-02 18:10:22 +0400

    fake server

--- test/irc2/irc_connect_test.rb
+++ test/irc2/irc_connect_test.rb
@@ -3,8 +3,24 @@ require_relative "irc_client"
 
 class IrcConnectTest < Minitest::Test
   def test_server_connection
-    client = PersistentIrcClient.new("irc.libera.chat", 6667)
+    FakeIrcServer.new
+
+    client = PersistentIrcClient.new("localhost", 6667)
 
     assert client.connected
+  end
+end
+
+class FakeIrcServer
+  def initialize
+    @socket = TCPServer.new("localhost", 6667)
+    Thread.new do
+      loop do
+        client = @socket.accept
+        client.puts ":localhost NOTICE * :*** Checking Ident"
+        client.puts ":localhost NOTICE * :*** Looking up your hostname..."
+        client.puts ":localhost NOTICE * :*** Couldn't look up your hostname"
+      end
+    end
   end
 end
