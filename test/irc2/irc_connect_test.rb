a905c1b48d6770fdf0346f8544a54b46a64e8562
Pankaj Sharma <sharmapankaj2512@gmail.com>
2023-09-02 18:14:24 +0400

    xm

--- test/irc2/irc_connect_test.rb
+++ test/irc2/irc_connect_test.rb
@@ -15,15 +15,25 @@ end
 class FakeIrcServer
   include Singleton
 
+  def handle_client(client)
+    welcome_messages(client)
+  end
+
   def initialize
     @socket = TCPServer.new("localhost", 6667)
     Thread.new do
       loop do
         client = @socket.accept
-        client.puts ":localhost NOTICE * :*** Checking Ident"
-        client.puts ":localhost NOTICE * :*** Looking up your hostname..."
-        client.puts ":localhost NOTICE * :*** Couldn't look up your hostname"
+        handle_client(client)
       end
     end
+  end
+
+  private
+
+  def welcome_messages(client)
+    client.puts ":localhost NOTICE * :*** Checking Ident"
+    client.puts ":localhost NOTICE * :*** Looking up your hostname..."
+    client.puts ":localhost NOTICE * :*** Couldn't look up your hostname"
   end
 end
